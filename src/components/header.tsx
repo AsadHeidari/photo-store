139ab11a2799d095c70c9d7d8a404b9bb3f03cb6
asadHeidari <asadheidari86@gmail.com>
2024-01-01 09:59:25 +0330

    Replaced app directory to the src.

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className="w-full bg-white h-32">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+    </div>
+  );
+}
+
+export default Header;
